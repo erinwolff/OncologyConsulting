303660a057feca3e10629ce68c443879f4f3e40a
Erin Wolff <erin_wolff@outlook.com>
2024-01-01 15:55:38 -0800

    adjusted size of font in navbar and changed to filled variant

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Card, Image, Flex, Box, Heading, Button } from '@chakra-ui/react'
+import { Card, Image, Flex, Box, Heading, Button, Spacer } from '@chakra-ui/react'
 import { EmailIcon, InfoIcon, CheckIcon } from '@chakra-ui/icons'
 import { Link as ReactRouterLink } from 'react-router-dom'
 import { Link as ChakraLink } from '@chakra-ui/react'
@@ -9,10 +9,11 @@ export default function Navbar() {
       <Card
         direction={{ base: 'column', sm: 'row' }}
         overflow='hidden'
-        variant='outline'
+        variant='filled'
+        p={5}
       >
-        <Flex align='center' justify='space-between' w='100%'>
-          <Box display='flex' alignItems='center'>
+        <Flex align='center' justify='space-between' w='100%' flexWrap='wrap'>
+          <Box display='flex' alignItems='center' mb={{ base: 4, sm: 0 }}>
             <Image
               objectFit='cover'
               boxSize='60px'
@@ -20,24 +21,24 @@ export default function Navbar() {
               alt='Logo'
               m={2}
             />
-            <Heading size='2xl'>Oncology Consulting Wolff LLC </Heading>
+            <Heading size='xl'>Oncology Consulting Wolff LLC </Heading>
           </Box>
 
-          <Flex align='center'>
+          <Flex align='center' justify={{ base: 'center', sm: 'flex-end' }} flex='1'>
             <ChakraLink as={ReactRouterLink} to='/home'>
-              <Button leftIcon={<InfoIcon />} colorScheme='telegram' variant='ghost' size='lg' m={2}>
+              <Button leftIcon={<InfoIcon />} colorScheme='blue' variant='solid' size='sm' m={1}>
                 About Us
               </Button>
             </ChakraLink>
 
             <ChakraLink as={ReactRouterLink} to='/services'>
-              <Button leftIcon={<CheckIcon />} colorScheme='telegram' variant='ghost' size='lg' m={2}>
+              <Button leftIcon={<CheckIcon />} colorScheme='blue' variant='solid' size='sm' m={1}>
                 Services
               </Button>
             </ChakraLink>
 
             <ChakraLink as={ReactRouterLink} to='/contact'>
-              <Button leftIcon={<EmailIcon />} colorScheme='telegram' variant='ghost' size='lg' m={2}>
+              <Button leftIcon={<EmailIcon />} colorScheme='blue' variant='solid' size='sm' m={1}>
                 Contact Us
               </Button>
             </ChakraLink>
