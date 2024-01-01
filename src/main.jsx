107c8cfff6b14ffb0812edde0a4a9548278f9a92
Erin Wolff <erin_wolff@outlook.com>
2024-01-01 14:04:23 -0800

    added paths for Services and Contact

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react'
 
 import './index.css';
 
 import Root from './layout/Root.jsx';
 import Home from './features/Home.jsx';
+import Services from './features/Services.jsx';
+import Contact from './features/Contact.jsx';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
@@ -12,12 +15,19 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
-    children: [{ path: '/', element: <Home /> }],
+    children: [
+      { path: '/', element: <Home /> },
+      { path: "/home", element: <Home /> },
+      { path: "/services", element: <Services /> },
+      { path: "/contact", element: <Contact /> },
+    ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </React.StrictMode>
 );
