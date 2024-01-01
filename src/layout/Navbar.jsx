284cf874097e984def8342df69427cb3bf7120a2
Erin Wolff <erin_wolff@outlook.com>
2024-01-01 15:11:54 -0800

    styling for navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,9 +1,9 @@
-import { Card, Image, Stack, CardBody, Heading, Text } from '@chakra-ui/react'
+import { Card, Image, Flex, Box, Heading, Button } from '@chakra-ui/react'
+import { EmailIcon, InfoIcon, CheckIcon } from '@chakra-ui/icons'
 import { Link as ReactRouterLink } from 'react-router-dom'
 import { Link as ChakraLink } from '@chakra-ui/react'
 
 export default function Navbar() {
-
   return (
     <>
       <Card
@@ -11,23 +11,38 @@ export default function Navbar() {
         overflow='hidden'
         variant='outline'
       >
-        <Image
-          objectFit='cover'
-          boxSize='100px'
-          src='/logo_favicon.png'
-          alt='Logo'
-        />
+        <Flex align='center' justify='space-between' w='100%'>
+          <Box display='flex' alignItems='center'>
+            <Image
+              objectFit='cover'
+              boxSize='60px'
+              src='/logo_favicon.png'
+              alt='Logo'
+              m={2}
+            />
+            <Heading size='2xl'>Oncology Consulting Wolff LLC </Heading>
+          </Box>
 
-        <Stack>
-          <CardBody>
-            <Heading size='lg'>Oncology Consulting Wolff LLC </Heading>
-            <Text fontSize='xl'>
-              <ChakraLink as={ReactRouterLink} to='/home'>About Us</ChakraLink> | 
-              <ChakraLink as={ReactRouterLink} to='/services'> Services</ChakraLink> | 
-              <ChakraLink as={ReactRouterLink} to='/contact'> Contact Us</ChakraLink>
-            </Text>
-          </CardBody>
-        </Stack>
+          <Flex align='center'>
+            <ChakraLink as={ReactRouterLink} to='/home'>
+              <Button leftIcon={<InfoIcon />} colorScheme='telegram' variant='ghost' size='lg' m={2}>
+                About Us
+              </Button>
+            </ChakraLink>
+
+            <ChakraLink as={ReactRouterLink} to='/services'>
+              <Button leftIcon={<CheckIcon />} colorScheme='telegram' variant='ghost' size='lg' m={2}>
+                Services
+              </Button>
+            </ChakraLink>
+
+            <ChakraLink as={ReactRouterLink} to='/contact'>
+              <Button leftIcon={<EmailIcon />} colorScheme='telegram' variant='ghost' size='lg' m={2}>
+                Contact Us
+              </Button>
+            </ChakraLink>
+          </Flex>
+        </Flex>
       </Card>
     </>
   );
