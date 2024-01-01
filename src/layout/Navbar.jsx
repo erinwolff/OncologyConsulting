6ec98e350659d9f9150e73f5db69d732037156be
Erin Wolff <erin_wolff@outlook.com>
2024-01-01 14:05:11 -0800

    added links to Navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,12 +1,34 @@
-import "./Navbar.less";
+import { Card, Image, Stack, CardBody, Heading, Text } from '@chakra-ui/react'
+import { Link as ReactRouterLink } from 'react-router-dom'
+import { Link as ChakraLink } from '@chakra-ui/react'
 
 export default function Navbar() {
-  
+
   return (
-    <nav> 
-      <menu class="navbar">
-       About Us | Services | Contact Us
-      </menu>
-    </nav>
+    <>
+      <Card
+        direction={{ base: 'column', sm: 'row' }}
+        overflow='hidden'
+        variant='outline'
+      >
+        <Image
+          objectFit='cover'
+          boxSize='100px'
+          src='/logo_favicon.png'
+          alt='Logo'
+        />
+
+        <Stack>
+          <CardBody>
+            <Heading size='lg'>Oncology Consulting Wolff LLC </Heading>
+            <Text fontSize='xl'>
+              <ChakraLink as={ReactRouterLink} to='/home'>About Us</ChakraLink> | 
+              <ChakraLink as={ReactRouterLink} to='/services'> Services</ChakraLink> | 
+              <ChakraLink as={ReactRouterLink} to='/contact'> Contact Us</ChakraLink>
+            </Text>
+          </CardBody>
+        </Stack>
+      </Card>
+    </>
   );
 }
