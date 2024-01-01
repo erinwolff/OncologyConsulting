83cfa78b7394244136c717648e0093aac097544f
Erin Wolff <erin_wolff@outlook.com>
2023-12-31 17:38:08 -0800

    initial commit

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,12 @@
+import "./Navbar.less";
+
+export default function Navbar() {
+  
+  return (
+    <nav> 
+      <menu>
+       About Us | Services | Contact Us
+      </menu>
+    </nav>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+
+import Root from './layout/Root.jsx';
+import Home from './features/Home.jsx';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [{ path: '/', element: <Home /> }],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
