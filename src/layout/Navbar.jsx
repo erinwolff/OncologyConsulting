6c972d40fdefee74eaf85a3c986c73d5cc5ae1c9
Erin Wolff <erin_wolff@outlook.com>
2024-01-03 16:36:39 -0800

    added more responsiveness to Navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,51 +1,49 @@
-import { Card, Image, Flex, Box, Heading, Button } from '@chakra-ui/react'
-import { EmailIcon, InfoIcon } from '@chakra-ui/icons'
-import { Link as ReactRouterLink } from 'react-router-dom'
-import { Link as ChakraLink, Icon } from '@chakra-ui/react'
+import { Card, Image, Flex, Box, Heading, Button } from '@chakra-ui/react';
+import { EmailIcon, InfoIcon } from '@chakra-ui/icons';
+import { Link as ReactRouterLink } from 'react-router-dom';
+import { Link as ChakraLink, Icon } from '@chakra-ui/react';
 import { LiaHandshake } from "react-icons/lia";
 
 export default function Navbar() {
   return (
-    <>
-      <Card
-        direction={{ base: 'column', sm: 'row' }}
-        overflow='hidden'
-        variant='filled'
-        p={5}
-      >
-        <Flex align='center' justify='space-between' w='100%' flexWrap='wrap'>
-          <Box display='flex' alignItems='center' mb={{ base: 4, sm: 0 }}>
-            <Image
-              objectFit='cover'
-              boxSize='60px'
-              src='/logo_favicon.png'
-              alt='Logo'
-              m={2}
-            />
-            <Heading size='xl'>Oncology Consulting Wolff LLC </Heading>
-          </Box>
+    <Card
+      direction={{ base: 'column', sm: 'row' }}
+      overflow='hidden'
+      variant='filled'
+      p={5}
+    >
+      <Flex align='center' justify='space-between' w='100%' flexWrap='wrap'>
+        <Box display='flex' alignItems='center' mb={{ base: 4, sm: 0 }} textAlign={{ base: 'center', sm: 'left' }}>
+          <Image
+            objectFit='cover'
+            boxSize={{ base: '40px', sm: '60px' }}
+            src='/logo_favicon.png'
+            alt='Logo'
+            m={2}
+          />
+          <Heading size='xl'>Oncology Consulting Wolff LLC </Heading>
+        </Box>
 
-          <Flex align='center' justify={{ base: 'center', sm: 'flex-end' }} flex='1'>
-            <ChakraLink as={ReactRouterLink} to='/home'>
-              <Button leftIcon={<InfoIcon />} colorScheme='blue' variant='solid' size='sm' m={1}>
-                About Us
-              </Button>
-            </ChakraLink>
+        <Flex align='center' justify={{ base: 'center', sm: 'flex-end' }} flex='1' mt={{ base: 4, sm: 0 }}>
+          <ChakraLink as={ReactRouterLink} to='/home'>
+            <Button leftIcon={<InfoIcon />} colorScheme='blue' variant='solid' size='sm' m={1}>
+              About Us
+            </Button>
+          </ChakraLink>
 
-            <ChakraLink as={ReactRouterLink} to='/services'>
-              <Button colorScheme='blue' variant='solid' size='sm' m={1}>
-                <Icon as={LiaHandshake} boxSize={6} mr={2}/>Services
-              </Button>
-            </ChakraLink>
+          <ChakraLink as={ReactRouterLink} to='/services'>
+            <Button colorScheme='blue' variant='solid' size='sm' m={1}>
+              <Icon as={LiaHandshake} boxSize={{ base: 4, sm: 6 }} mr={2}/>Services
+            </Button>
+          </ChakraLink>
 
-            <ChakraLink as={ReactRouterLink} to='/contact'>
-              <Button leftIcon={<EmailIcon />} colorScheme='blue' variant='solid' size='sm' m={1}>
-                Contact Us
-              </Button>
-            </ChakraLink>
-          </Flex>
+          <ChakraLink as={ReactRouterLink} to='/contact'>
+            <Button leftIcon={<EmailIcon />} colorScheme='blue' variant='solid' size='sm' m={1}>
+              Contact Us
+            </Button>
+          </ChakraLink>
         </Flex>
-      </Card>
-    </>
+      </Flex>
+    </Card>
   );
 }
