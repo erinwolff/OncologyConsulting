2e2e029e8fdbb1107aebba4c2e5975971a85dd8e
Erin Wolff <erin_wolff@outlook.com>
2024-01-04 12:11:43 -0800

    updated behavior for Flex from sm to lg

--- src/features/Home.jsx
+++ src/features/Home.jsx
@@ -45,7 +45,7 @@ export default function Home() {
         mx={{ base: 4, sm: 'auto' }}
       >
         <Flex
-          direction={{ base: 'column', sm: 'row' }}
+          direction={{ base: 'column', lg: 'row' }}
           align='center'
           justify='center'
         >
@@ -58,7 +58,7 @@ export default function Home() {
           <Stack spacing='3' mt={{ base: 5, sm: 0 }} ml={{ sm: 4 }}>
             <Heading size='xl'>Johannes Wolff</Heading>
             <Text fontSize='2xl'>Founder and CMO</Text>
-            <Text gridColumn={{ sm: 'span 2' }}>
+            <Text>
               Dr med habil Johannes E Wolff, MD PhD is an accomplished medical researcher, physician, and drug developer. He has completed medical training in Germany to be board certified pediatric hematologist oncologist. His medical practice of three decades was in Europe, Canada, and the United States, and he considers among all these places MDAnderson Cancer Center in Houston his academic home. He developed, wrote, led, and published international cooperative clinical trials, tested novel drugs in his laboratories, taught biostatistics, spearheaded immune therapy, and personalized targeted therapy.
               <br />
               <br />His academic titles included the title of professor in four different universities, and his administrative roles went beyond staff physician and medical faculty to section head (MDAnderson), divisions chief (Tufts), and department chair (Cleveland Clinic). His move to the pharmaceutical industry came after over 200 peer-reviewed publications, and it started in Medical Affairs in Novartis. Within the pharmaceutical industry, Johannes Wolff calls AbbVie his home, where he worked in late development (Venetoclax), early oncology development, and safety. After moving to smaller companies to become proficient in the business agility necessary for resource-poor environments, he now runs his own company allowing to independently distribute his experience wider.
